af7bcce08b2972a491a35ded7ea473f30c88494f
Pablo <pablogroza2000@gmail.com>
2025-05-14 16:21:57 +0200

    Segundo Commit Pokedex

--- pokemon-details.js
+++ pokemon-details.js
@@ -1,4 +1,4 @@
-const currentPokemonID = null
+let currentPokemonID = null
 
 document.addEventListener("DOMContentLoaded", () => {
     const MAX_POKEMONS = 151
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     currentPokemonID = id
-    localPokemon(id)
+    loadPokemons(id)
 })
 
 async function loadPokemons(id){
@@ -21,13 +21,92 @@ async function loadPokemons(id){
       fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json()),
       fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) => res.json())
     ]);
-    const abilitiesWrap =document.querySelector(".pokemon-detail-wrap .pokemon-detail-move")
-    abilitiesWrap.innerHTML = ""
+    const abilitiesWrapper = document.querySelector(".pokemon-detail-wrap .pokemon-detail-move")
+    abilitiesWrapper.innerHTML = ""
+    if(currentPokemonID === id){
+        showPokemonDetails(pokemon)
+        const flavorText = getEnglishFavorText(pokemon-Species)
+        document.querySelector(".body3-fonts.pokemon-description").textContent = flavorText
+        
+        const [leftArrow, rightArrow] = ["#leftArrow", "#righttArrow"].map((sel) =>
+        document.querySelector(sel))
+
+        leftArrow.removeEventListener("click", explorePokemon)
+        rightArrow.removeEventListener("click", explorePokemon)
+
+        if(id !== 1){
+            leftArrow.addEventListener("click", () => {
+                explorePokemon(id-1)
+            })
+        }
+        if(id !== 151) {
+            rightArrow.addEventListener("click", () =>{
+                explorePokemon(id+1)
+            })
+        }
+        window.history.pushState({}, "", `./detail.html?id=${id}`)
+        }
+
         return true
     }
 
     catch(error){
-        console.error("An error has ocurred while fetching Pokemons data:", error)
+        console.log("An error has ocurred while fetching Pokemons data:", error)
             return false
     }
+}
+
+async function explorePokemon(id){
+    currentPokemonID = id
+    await loadPokemons(id)
+}
+
+const pokemonColors = {
+    normal: "A8A878",
+    water: "A8A878",
+    grass: "A8A878",
+    bug: "A8A878",
+    fire: "A8A878",
+    dragon: "A8A878",
+    flying: "A8A878",
+    electric: "A8A878",
+    rock: "A8A878",
+    steel: "A8A878",
+    fairy: "A8A878",
+    dark: "A8A878",
+    ghost: "A8A878",
+    poison: "A8A878",
+    fighting: "A8A878",
+    ice: "A8A878",
+    ground: "A8A878",
+    psychic: "A8A878",
+}
+
+function applyElementStyles(elements, cssProperty, value){
+    elements.forEach((element) => {
+        element.style[cssProperty] = value
+    })
+}
+
+function rgbaFromHex(hexColor) {
+    return [
+        parseInt(hexColor.slice(1, 3), 16),
+        parseInt(hexColor.slice(3, 5), 16),
+        parseInt(hexColor.slice(5, 7), 16),
+    ].join(", ")
+}
+
+function applyBackgroundColor(pokemon){
+    const mainType = pokemon.types[0].type.name
+    const color = pokemonColors[mainType]
+
+    if(!color){
+        console.warn(`This color is not defined yet: ${mainType}`)
+        return
+    }
+    const detailingMainElement = document.querySelector(".detail-main")
+    applyElementStyles([detailingMainElement],
+    "background", color)
+    applyElementStyles([detailingMainElement],
+    "borderColor", color)
 }
